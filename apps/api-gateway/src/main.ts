8c904a87edb739fcac71cd172b36ff4bb12c6a09
its-sarath28 <sarathrs220@gmail.com>
2025-08-26 23:01:05 +0530

    Added CORS

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+  app.enableCors({ origin: ['http://localhost:5173'], credentials: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
